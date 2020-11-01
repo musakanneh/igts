721562d53bd1c571e82da30c69d0063389f1e751
musakanneh <mkanneh17@alustudent.com>
2020-11-01 22:02:37 +0200

    Updated folder structures + more dev

--- src/components/Offers.vue
+++ src/components/Offers.vue
@@ -1,40 +1,46 @@
 <template>
-<body>
+  <body>
     <main>
-        <section>
-            <div>
-                <h2>
-                    Digital Literacy Program
-                </h2>
-                <p>
-                    A two-weeks paid-for-training course designed to expose participants
-                    to different tech awareness courses, programming, and basic
-                    artificial intelligence - AI.
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Digital Literacy Program
-                </h2>
-                <p>
-                    A two-weeks paid-for-training course designed to expose participants
-                    to different tech awareness courses, programming, and basic
-                    artificial intelligence - AI.
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Digital Literacy Program
-                </h2>
-                <p>
-                    A two-weeks paid-for-training course designed to expose participants
-                    to different tech awareness courses, programming, and basic
-                    artificial intelligence - AI.
-                </p>
-            </div>
-        </section>
+      <section>
+        <div>
+          <img src="../assets/case.png" alt="" />
+          <h2>
+            Digital Literacy Program
+          </h2>
+          <p>
+            A two-weeks paid-for-training course designed to expose participants
+            to different tech awareness courses, programming, and basic
+            artificial intelligence - AI.
+          </p>
+          <a href="">Discover</a>
+        </div>
+        <div>
+          <img src="../assets/circle.png" alt="" />
+          <h2>
+            Digital Literacy Program
+          </h2>
+          <p>
+            One-stop-shop center established to provide top-notch and executive
+            ICT and tech/digital consultancies to individuals, INGOs/NGOs,
+            businesses and more
+          </p>
+            <a href="">Discover</a>
+        </div>
+        <div>
+          <img src="../assets/circle.png" alt="" />
+          <h2>
+            Digital Literacy Program
+          </h2>
+          <p>
+            IGTS engages into online and e-commerce business networking and
+            trade. We work with cooperatives, trade unions, and business groups
+            to facillitate markt expousre.
+          </p>
+            <a href="">Discover</a>
+        </div>
+      </section>
     </main>
-</body>
+  </body>
 </template>
 
 <script>
@@ -43,15 +49,38 @@ export default {};
 
 <style lang="scss" scoped>
 body {
-    main {
-        section {
-            display: flex;
-            justify-content: space-between;
-
-            div {
-                border: 1px solid red;
-            }
-        }
+  main {
+    img {
+      width: 25px;
     }
+
+    section {
+      justify-content: space-between;
+      width: 90%;
+      padding-top: 40px;
+      display: flex;
+      margin: auto;
+
+      h2 {
+        font-size: 14px;
+        padding: 10px;
+      }
+
+      div {
+        text-align: center;
+
+        p {
+          font-size: 12px;
+          width: 80%;
+          line-height: 20px;
+          margin: auto;
+        }
+        a{
+            text-decoration: none;
+            font-size: 12px;
+        }
+      }
+    }
+  }
 }
 </style>
