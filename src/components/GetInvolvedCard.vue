2d9d000576e88e13fcb5c6645c04d4c49dd590bb
musakanneh <mkanneh17@alustudent.com>
2020-11-12 23:56:08 +0200

    Added about page

---
+++ src/components/GetInvolvedCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <body>
+    <main>
+      <Caption captions="Get Involved wit our Programs"></Caption>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+        purus sit amet luctus venenatis, lectus magna
+      </p>
+    </main>
+  </body>
+</template>
+
+<script>
+import Caption from "@/components/Caption.vue";
+export default {
+  components: {
+    Caption,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,110 @@
 <template>
-<body>
-    <h1>
-        About
-    </h1>
-</body>
+  <body>
+    <main>
+      <div class="sec-one-cover">
+        <div class="cap">
+          <h1>
+            Your awesome IT Tech <br />
+            needs consultant
+          </h1>
+          <p>
+            The Internet Generation Technology Solutions (IGTS) Africa Limited
+            is building 100 software engineers by 2023 through the techaware
+            fellowship program that will use ICT for sustainable development.
+          </p>
+          <br />
+          <p>
+            We are an Information Communication Technology (ICT) firm/company
+            that promotes digital inclusivity and awareness in Liberia through
+            digital literacy programs, software designs and development, online
+            marketing and branding, educational tech shows and digital product
+            sales and merchandise.
+          </p>
+          <br />
+          <p>
+            IGTS intervention will focus on key areas beginning with the Digital
+            Literacy Program (DLP) through the Tech Aware Crash Courses and the
+            Tech Aware Fellowship Program; Tech Awareness through educational
+            Tech Shows; Tech consultancy to individuals, institutions & others
+            through our ICT-Tech Help Clinic; and ICT solutions and innovations
+            through Apps development, Online Merchandising, and
+            e-Commerce/trade;
+          </p>
+          <br />
+          <Button
+            :isFullWidth="false"
+            label="Explore More"
+            width="150px"
+          ></Button>
+        </div>
+        <div class="img-cover">
+          <img src="../assets/code.jpeg" alt="" />
+        </div>
+      </div>
+      <GetInvolvedCard />
+    </main>
+  </body>
 </template>
 
 <script>
-export default {};
+import Button from "@/components/Button.vue";
+import GetInvolvedCard from "@/components/GetInvolvedCard.vue";
+export default {
+  name: "Services",
+  components: {
+    Button,
+    GetInvolvedCard,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+body {
+  background-color: #fbfbfb;
+  display: flex;
+  flex-direction: column;
+  flex: auto;
+  width: 100%;
+
+  p {
+    font-size: 15px;
+    line-height: 22px;
+    color: #303030;
+  }
+}
+
+.sec-one-cover {
+  display: flex;
+  flex-direction: row;
+  flex: auto;
+  margin: auto;
+  padding: 4% 0;
+  width: 95%;
+  justify-content: space-between;
+
+  h1 {
+    padding: 25px 0;
+    font-size: 35px;
+    color: #3b85fc;
+    font-weight: 600;
+    transition: 200ms;
+    white-space: nowrap;
+  }
+
+  .cap {
+    width: 40%;
+
+    p {
+      line-height: 22px;
+      font-size: 15px;
+    }
+  }
+
+  .img-cover {
+    width: 50%;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
