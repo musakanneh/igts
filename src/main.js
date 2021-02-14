8cf571b30e86d9c905061de2f34d4ccb223297d6
ianodhiambo9 <ianodhiambo9@gmail.com>
2021-02-14 18:15:16 +0200

    added the vue text transition library

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import './js/slider.js'
 
+// animation library
+import VueTextTransition from 'vue-text-transition'
+Vue.component('vue-text-transition', VueTextTransition)
+
 Vue.config.productionTip = false;
 
 new Vue({
